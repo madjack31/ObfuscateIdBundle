706e8e47a0b7a4fbbc05ba8e2c424d41d487d6c9
madjack31 <c.verbeke@free.fr>
2024-11-05 09:45:34 +0100

    Update ObfuscateIdValueResolver.php

--- src/ValueResolver/ObfuscateIdValueResolver.php
+++ src/ValueResolver/ObfuscateIdValueResolver.php
@@ -37,17 +37,24 @@ final readonly class ObfuscateIdValueResolver implements ValueResolverInterface
 
 	private function deobfuscateAndHydrateEntity($request, $attribute): array
 	{
+		/*
 		if ($request->attributes->get($attribute->routeParam) === null) {
 			throw new \InvalidArgumentException(sprintf('Some mandatory parameters are missing "%s" to deobfuscate', $attribute->routeParam));
 		}
+		*/
 		$value = $this->obfuscate->deobfuscate($request->attributes->get($attribute->routeParam));
 
+		/*
 		if ($value === null) {
 			throw new \InvalidArgumentException(sprintf('This value %s cannot be deobfuscated', $request->attributes->get($attribute->routeParam)));
 		}
-		$entity = $this->entityManager->getRepository($attribute->entity)->findOneBy([$attribute->identifierField => $value]);
-		if (!$entity) {
-			throw new NotFoundHttpException(sprintf('"%s" object not found by "%s".', $attribute->entity, self::class));
+		*/
+		$entity = null;
+		if ($value !== null) {
+			$entity = $this->entityManager->getRepository($attribute->entity)->findOneBy([$attribute->identifierField => $value]);
+			if (!$entity) {
+				throw new NotFoundHttpException(sprintf('"%s" object not found by "%s".', $attribute->entity, self::class));
+			}
 		}
 		return [$entity] ?? [];
 	}
