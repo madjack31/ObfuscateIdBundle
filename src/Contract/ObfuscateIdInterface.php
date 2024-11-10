9a2ed5ee59f2c3013b7f63b0655a9c0e8f8e2de0
madjack31 <c.verbeke@free.fr>
2024-11-10 09:27:02 +0100

    Allow to return null values

--- src/Contract/ObfuscateIdInterface.php
+++ src/Contract/ObfuscateIdInterface.php
@@ -4,7 +4,7 @@ namespace Zepekegno\ObfuscateIdBundle\Contract;
 
 interface ObfuscateIdInterface
 {
-	public function obfuscate(?int $value): string;
+	public function obfuscate(?int $value): ?string;
 
 	public function deobfuscate(?string $value): ?int;
 
